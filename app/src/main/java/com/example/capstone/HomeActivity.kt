600d0a06a8c3220193707c2b7fe88884ebb88eb2
deliyaizztl <deliyaizztl@gmail.com>
2023-06-14 21:42:25 +0700

    commit mau finish

--- app/src/main/java/com/example/capstone/HomeActivity.kt
+++ app/src/main/java/com/example/capstone/HomeActivity.kt
@@ -1,31 +1,100 @@
 package com.example.capstone
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.capstone.databinding.ActivityHomeBinding
+import java.io.File
 
-class HomeActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        val textViewBMIText : TextView = findViewById(R.id.tv_bmi_text)
-        val imgvCamera : ImageView = findViewById(R.id.imgv_camera)
-        val btnTest : Button = findViewById(R.id.btn_test)
-        btnTest.setOnClickListener(this)
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btn_test -> {
-                val moveTest = Intent(this@HomeActivity, ResultActivity::class.java)
-                startActivity(moveTest)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
             }
         }
+    }
 
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.cameraXButton.setOnClickListener { startCameraX() }
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.uploadButton.setOnClickListener { uploadImage() }
+    }
+
+    private fun uploadImage() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startGallery() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startTakePhoto() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                it.data?.getSerializableExtra("picture", File::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                it.data?.getSerializableExtra("picture")
+            } as? File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            myFile?.let { file ->
+                rotateFile(file, isBackCamera)
+                binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(moveLogin)
                 }
 
-            
+
         }
         when (v?.id) {
             R.id.btn_register -> {
